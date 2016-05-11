d6d0b39dfeadabaf7432f4fcc1b2656b2070ba4a
DylanAuty <dylan.auty@gmail.com>
2016-05-11 20:18:28 +0100

    Made a module that prints dimensions of passing tensors

---
+++ dimPrint.lua
@@ -0,0 +1,19 @@
+--[[ dimPrint.lua
+-- A torch nn module that can be inserted into forward networks.
+-- To be used to track the dimensions of tensors in a network.
+-- Dylan Auty, May 2016
+--]]
+
+require 'torch'
+require 'nn'
+
+local DP, parent = torch.class('nn.dimPrint', 'nn.Module')
+
+function DP:__init()
+	parent.__init(self)
+
+end
+
+function DP:updateOutput(input)
+	print("SIZE: ", #input)
+end
